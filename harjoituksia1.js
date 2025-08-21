c4b8996d20d850979e74949402358e5bc1ddd20f
Simppa <simo.siren@careeria.fi>
2025-08-21 12:03:09 +0300

    silmukka yms

---
+++ harjoituksia1.js
@@ -0,0 +1,15 @@
+console.log("Hello!")
+
+// Let on muuttuja
+// Const on vakiomuuttuja
+// Var on muuttuja, jota käytetään harvemmin jos let
+//..näkyvyys ei riitä
+
+ikä = 15
+
+if (ikä < 18) {
+    console.log("kyseessä on alaikäinen")
+}
+else {
+    console.log("kyseessä on täysikäinen")
+}
---
+++ silmukat.js
@@ -0,0 +1,13 @@
+
+const puhelimet = ["Samsung", "Oppo", "One plus", "Honor" ]
+
+// Foreach silmukka käy koko taulukon läpi
+// puh on alias nimi jokaiselle vuorollaan silmukassa
+puhelimet.forEach(puh => {
+    console.log(puh)
+})
+
+// Suorita: node silmukat.js
+
+
+
