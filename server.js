491fe1faa14cac24835627a301e4da8ff2973083
Simppa <simo.siren@careeria.fi>
2025-09-01 10:37:42 +0300

    html sivun lähetys

--- server.js
+++ server.js
@@ -6,10 +6,18 @@
 // Luodaan yksinkertainen Express-palvelin
 const express = require('express')
 const app = express()
-const PORT = 3000
+const PORT = 3000 || process.env.PORT
+const path = require('path') //////////////
+
+// Tehdään polkumääritys front kansioon 
+const polku = path.join(__dirname, '/front')////////////////
+
+// Sanotaan että em. polussa on tiedostosisältö jota palvelin käyttää kun se saa http requestin
+app.use(express.static(polku))///////////////
+
 
 // Reititys
-app.get('/', (req, res) => {
+app.get('/hello', (req, res) => {
   res.send('Hello World!')
 })
 
