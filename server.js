30f4193d1c1ca1106305729ee68aeec8139a82a9
Simppa <simo.siren@careeria.fi>
2025-09-01 10:00:22 +0300

    express server luotu

---
+++ server.js
@@ -0,0 +1,20 @@
+
+// Luodaan yksinkertainen Express-palvelin
+const express = require('express')
+const app = express()
+const PORT = 3000
+
+// Reititys
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/suomeksi', (req, res) => {
+  res.send('Hei Maailma!')
+})
+
+
+// Käynnistetään palvelin
+app.listen(PORT, () => {
+  console.log("Server is running on http://localhost:" + PORT)
+})
